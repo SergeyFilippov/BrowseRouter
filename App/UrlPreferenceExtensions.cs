69cfd4585a7f3421f0badda24a7f8ae7c061748d
Sergey Filippov <s.filippov@raynet.de>
2022-08-29 16:30:34 +0200

    Recursive url preference matching

--- App/UrlPreferenceExtensions.cs
+++ App/UrlPreferenceExtensions.cs
@@ -17,28 +17,80 @@ public static class UrlPreferenceExtensions
 
   public static (string, string) GetDomainAndPattern(this UrlPreference pref, Uri uri)
   {
+      (string? domain, string? pattern, bool isRegex) = GetDomainAndPatternRecursive(pref, uri, false);
+
+      if(!isRegex)
+      {
+          // We're only checking the domain.
+          domain = domain ?? uri.Authority;
+
+          // Escape the input for regex; the only special character we support is a *
+          var regex = Regex.Escape(pattern??pref.UrlPattern);
+
+          // Unescape * as a wildcard.
+          pattern = $"^{regex.Replace("\\*", ".*")}$";
+      }
+
+      if (domain == null)
+      {
+          throw new ApplicationException("Domain value is invalid.");
+      }
+
+      if (pattern == null)
+      {
+          throw new ApplicationException("Patter value is invalid.");
+      }
+
+      return (domain, pattern);
+  }
+
+  public static (string?, string?, bool) GetDomainAndPatternRecursive(this UrlPreference pref, Uri uri, bool isRegex)
+  {
     string urlPattern = pref.UrlPattern;
+
+    string? domain = null;
+    string? pattern = null;
+    bool isRegexPattern = isRegex;
+
+    bool isProcessed = false;
 
     if (urlPattern.StartsWith("/") && urlPattern.EndsWith("/"))
     {
-      // The domain from the INI file is a regex
-      string domain = uri.Authority + uri.AbsolutePath;
-      string pattern = urlPattern.Substring(1, urlPattern.Length - 2);
-
-      return (domain, pattern);
+        // The domain from the INI file is a regex
+        domain = uri.Authority + uri.AbsolutePath;
+        pattern = urlPattern.Substring(1, urlPattern.Length - 2);
+        isRegexPattern = true;
+        isProcessed = true;
     }
 
+    if (urlPattern.StartsWith("?") && urlPattern.EndsWith("?"))
     {
-      // We're only checking the domain.
-      string domain = uri.Authority;
-
-      // Escape the input for regex; the only special character we support is a *
-      var regex = Regex.Escape(urlPattern);
-
-      // Unescape * as a wildcard.
-      string pattern = $"^{regex.Replace("\\*", ".*")}$";
-
-      return (domain, pattern);
+        // The domain from the INI file is a regex
+        domain = uri.Authority + uri.PathAndQuery;
+        pattern = urlPattern.Substring(1, urlPattern.Length - 2);
+        isProcessed = true;
     }
+
+    if (isProcessed)
+    {
+        var innerResult = 
+            GetDomainAndPatternRecursive(
+            new UrlPreference
+            {
+                Browser = pref.Browser, 
+                UrlPattern = pattern
+            }, 
+            UriFactory.Get(domain), 
+            isRegexPattern);
+
+        if (!string.Equals(innerResult.Item1, domain) || !string.Equals(innerResult.Item2, pattern))
+        {
+            domain = innerResult.Item1 ?? domain;
+            pattern = innerResult.Item2 ?? pattern;
+            isRegexPattern = innerResult.Item3;
+        }
+    }
+
+    return (domain, pattern, isRegexPattern);
   }
 }
